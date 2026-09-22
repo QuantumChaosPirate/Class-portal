cc96f33b5950e9bc0688839c0fd3c03708942b91
danbuh07 <danielbuhagiar07@gmail.com>
2026-09-22 20:30:50 +0200

    Rename Year 9/10 to include Social Studies - General

--- js/config.js
+++ js/config.js
@@ -43,59 +43,59 @@ const CONFIG = {
     },
 
     year9: {
-      title: "Year 9", crumb: "Year 9",
+      title: "Year 9 Social Studies - General", crumb: "Year 9 Social Studies - General",
       children: ["year9_level123", "year9_ccp"]
     },
     year9_level123: {
-      title: "Level 1-2-3", crumb: "Year 9 &rsaquo; Level 1-2-3",
+      title: "Level 1-2-3", crumb: "Year 9 Social Studies - General &rsaquo; Level 1-2-3",
       children: ["year9_level123_lessons", "year9_level123_resources", "year9_level123_noticeboard", "year9_level123_studyboards"]
     },
     year9_level123_lessons: {
-      title: "Lessons", type: "dynamic", crumb: "Year 9 &rsaquo; Level 1-2-3 &rsaquo; Lessons",
+      title: "Lessons", type: "dynamic", crumb: "Year 9 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Lessons",
       folder: "year9_level123_lessons"
     },
-    year9_level123_resources: { title: "Resources", type: "files", crumb: "Year 9 &rsaquo; Level 1-2-3 &rsaquo; Resources", folder: "year9_level123_resources" },
-    year9_level123_studyboards: { title: "Study Boards", type: "files", crumb: "Year 9 &rsaquo; Level 1-2-3 &rsaquo; Study Boards", folder: "year9_level123_studyboards" },
-    year9_level123_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 9 &rsaquo; Level 1-2-3 &rsaquo; Noticeboard", noticeClass: "year9l123" },
+    year9_level123_resources: { title: "Resources", type: "files", crumb: "Year 9 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Resources", folder: "year9_level123_resources" },
+    year9_level123_studyboards: { title: "Study Boards", type: "files", crumb: "Year 9 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Study Boards", folder: "year9_level123_studyboards" },
+    year9_level123_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 9 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Noticeboard", noticeClass: "year9l123" },
 
     year9_ccp: {
-      title: "CCP", crumb: "Year 9 &rsaquo; CCP",
+      title: "CCP", crumb: "Year 9 Social Studies - General &rsaquo; CCP",
       children: ["year9_ccp_lessons", "year9_ccp_resources", "year9_ccp_noticeboard", "year9_ccp_studyboards"]
     },
     year9_ccp_lessons: {
-      title: "Lessons", type: "dynamic", crumb: "Year 9 &rsaquo; CCP &rsaquo; Lessons",
+      title: "Lessons", type: "dynamic", crumb: "Year 9 Social Studies - General &rsaquo; CCP &rsaquo; Lessons",
       folder: "year9_ccp_lessons"
     },
-    year9_ccp_resources: { title: "Resources", type: "files", crumb: "Year 9 &rsaquo; CCP &rsaquo; Resources", folder: "year9_ccp_resources" },
-    year9_ccp_studyboards: { title: "Study Boards", type: "files", crumb: "Year 9 &rsaquo; CCP &rsaquo; Study Boards", folder: "year9_ccp_studyboards" },
-    year9_ccp_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 9 &rsaquo; CCP &rsaquo; Noticeboard", noticeClass: "year9ccp" },
+    year9_ccp_resources: { title: "Resources", type: "files", crumb: "Year 9 Social Studies - General &rsaquo; CCP &rsaquo; Resources", folder: "year9_ccp_resources" },
+    year9_ccp_studyboards: { title: "Study Boards", type: "files", crumb: "Year 9 Social Studies - General &rsaquo; CCP &rsaquo; Study Boards", folder: "year9_ccp_studyboards" },
+    year9_ccp_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 9 Social Studies - General &rsaquo; CCP &rsaquo; Noticeboard", noticeClass: "year9ccp" },
 
     year10: {
-      title: "Year 10", crumb: "Year 10",
+      title: "Year 10 Social Studies - General", crumb: "Year 10 Social Studies - General",
       children: ["year10_level123", "year10_ccp"]
     },
     year10_level123: {
-      title: "Level 1-2-3", crumb: "Year 10 &rsaquo; Level 1-2-3",
+      title: "Level 1-2-3", crumb: "Year 10 Social Studies - General &rsaquo; Level 1-2-3",
       children: ["year10_level123_lessons", "year10_level123_resources", "year10_level123_noticeboard", "year10_level123_studyboards"]
     },
     year10_level123_lessons: {
-      title: "Lessons", type: "dynamic", crumb: "Year 10 &rsaquo; Level 1-2-3 &rsaquo; Lessons",
+      title: "Lessons", type: "dynamic", crumb: "Year 10 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Lessons",
       folder: "year10_level123_lessons"
     },
-    year10_level123_resources: { title: "Resources", type: "files", crumb: "Year 10 &rsaquo; Level 1-2-3 &rsaquo; Resources", folder: "year10_level123_resources" },
-    year10_level123_studyboards: { title: "Study Boards", type: "files", crumb: "Year 10 &rsaquo; Level 1-2-3 &rsaquo; Study Boards", folder: "year10_level123_studyboards" },
-    year10_level123_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 10 &rsaquo; Level 1-2-3 &rsaquo; Noticeboard", noticeClass: "year10l123" },
+    year10_level123_resources: { title: "Resources", type: "files", crumb: "Year 10 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Resources", folder: "year10_level123_resources" },
+    year10_level123_studyboards: { title: "Study Boards", type: "files", crumb: "Year 10 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Study Boards", folder: "year10_level123_studyboards" },
+    year10_level123_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 10 Social Studies - General &rsaquo; Level 1-2-3 &rsaquo; Noticeboard", noticeClass: "year10l123" },
 
     year10_ccp: {
-      title: "CCP", crumb: "Year 10 &rsaquo; CCP",
+      title: "CCP", crumb: "Year 10 Social Studies - General &rsaquo; CCP",
       children: ["year10_ccp_lessons", "year10_ccp_resources", "year10_ccp_noticeboard", "year10_ccp_studyboards"]
     },
     year10_ccp_lessons: {
-      title: "Lessons", type: "dynamic", crumb: "Year 10 &rsaquo; CCP &rsaquo; Lessons",
+      title: "Lessons", type: "dynamic", crumb: "Year 10 Social Studies - General &rsaquo; CCP &rsaquo; Lessons",
       folder: "year10_ccp_lessons"
     },
-    year10_ccp_resources: { title: "Resources", type: "files", crumb: "Year 10 &rsaquo; CCP &rsaquo; Resources", folder: "year10_ccp_resources" },
-    year10_ccp_studyboards: { title: "Study Boards", type: "files", crumb: "Year 10 &rsaquo; CCP &rsaquo; Study Boards", folder: "year10_ccp_studyboards" },
-    year10_ccp_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 10 &rsaquo; CCP &rsaquo; Noticeboard", noticeClass: "year10ccp" }
+    year10_ccp_resources: { title: "Resources", type: "files", crumb: "Year 10 Social Studies - General &rsaquo; CCP &rsaquo; Resources", folder: "year10_ccp_resources" },
+    year10_ccp_studyboards: { title: "Study Boards", type: "files", crumb: "Year 10 Social Studies - General &rsaquo; CCP &rsaquo; Study Boards", folder: "year10_ccp_studyboards" },
+    year10_ccp_noticeboard: { title: "Noticeboard", type: "notice", crumb: "Year 10 Social Studies - General &rsaquo; CCP &rsaquo; Noticeboard", noticeClass: "year10ccp" }
   }
 };
